3647138a30b4d117a6b11192516234aec9cf879d
Alex Scott <ascott011uk@gmail.com>
2025-12-03 11:36:07 -0500

    Exercise 1 complete

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -19,7 +19,20 @@ def main():
     # check for validity of urls before downloading
     # extract csv file from zip
     # rename csv file to just file name without path
-    pass
+    if not os.path.exists("downloads"):
+        os.mkdir("downloads")
+    for uri in download_uris:
+        response = requests.head(uri)
+        if response.status_code == 200:
+            print(f"Downloading from {uri}...")
+            r = requests.get(uri)
+            file_name = uri.split("/")[-1]
+            zip_path = os.path.join("downloads", file_name)
+            with open(zip_path, "wb") as f:
+                f.write(r.content)
+            print(f"Downloaded {file_name} to downloads/")
+        else:
+            print(f"URL {uri} is not valid. Skipping download.")
 
 
 if __name__ == "__main__":
